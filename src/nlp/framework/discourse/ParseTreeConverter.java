5dee58319d187ca24d25af4a7037e57ce6b04afe
karins <karinsim@gmail.com>
2014-11-19 21:18:01 +0000

    updated to stream

--- src/nlp/framework/discourse/ParseTreeConverter.java
+++ src/nlp/framework/discourse/ParseTreeConverter.java
@@ -1,7 +1,7 @@
 package nlp.framework.discourse;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
@@ -12,6 +12,12 @@ import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 
+/** 
+ * Converts text with document markup into parse trees, using Stanford CoreNLP.
+ *  
+ * @author Karin
+ *
+ */
 public class ParseTreeConverter {
 
 	//private static final String XML_START = "<?xml version="1.0" encoding="UTF-8"?><srcset  srclang=\"any\">";
@@ -29,15 +35,75 @@ public class ParseTreeConverter {
 	 * This will write to one outputfile with parse trees for each document seperated by doc tags 
 	 */
 	public static void main(String [] args){
-		String filename = args[0];
-		String outputfile = args[1];
+		//String filename = args[0];
+		//String outputfile = args[1];
 		
 		ParseTreeConverter converter = new ParseTreeConverter(); 
-		converter.parse(filename, outputfile);
+		//converter.parse(filename, outputfile);
+		converter.parse();
 	}
 	
+	/**
+	 * Parses documents into parse trees.
+	 * Input and output from console
+	 */
+	public void parse(){
+		
+		this.pipeline = new StanfordCoreNLP(new Properties());
+		//Map<String, List<String>> docsAndIds = new CorpusReader().readXMLfromConsole(true);
+		Map<String, String> docAndIds = new CorpusReader().readXMLfromConsole();
+		
+		//List<String> docs =  docsAndIds.get("docs");
+		//List<String> ids =  docsAndIds.get("ids");
+		
+		System.out.println(XML_START);
+		//for (int i = 0; i<docs.size(); i++){
+		//for(String docAsString: docs){
+		for(String id : docAndIds.keySet()){
+			
+			printParseTree(docAndIds.get(id), id);
+			
+		}
+		System.out.println(XML_END);
+		
+	}
 	
+	/**
+	 * For each document:
+	 * <li>		Read in source text and construct parse tree. 
+	 * <li>		 
+	 * @param id 
+	 *  @return parse tree
+	 * @param 
+	 */
+	public void printParseTree(String docAsString, String id){
+		
+		
+		Annotation document = new Annotation(docAsString);
+		this.pipeline.annotate(document);
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);		
+		
+		
+		printStartTag(id);
+		for(CoreMap sentence: sentences) {
+			
+			// this is the parse tree of the current sentence
+			Tree root = sentence.get(TreeAnnotation.class);
+			
+			System.out.println(root);
+			
+		}
+		System.out.println(DOC_TAG_END);
+	}
+
 	
+	private void printStartTag(String id) {
+		// TODO Auto-generated method stub
+		//System.out.println(DOC_TAG_START);
+		System.out.println("\n<doc id=\""+id+"\">\n");
+		
+	}
+
 	/**
 	 * Parses documents into parse trees.
 	 * @param filename of file to be parsed. this contains documents with doc tag boundaries
@@ -72,9 +138,6 @@ public class ParseTreeConverter {
 	 */
 	public StringBuffer getParseTree(String docAsString, StringBuffer trees){
 		
-		
-		//TODO: DONT CALL THIS EACH TIME !!! 
-		//List<CoreMap> sentences = getAnnotatedDocument(docAsString);
 		Annotation document = new Annotation(docAsString);
 		this.pipeline.annotate(document);
 		List<CoreMap> sentences = document.get(SentencesAnnotation.class);		
